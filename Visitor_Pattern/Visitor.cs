a65a701fffd01b8c35a9c260d5f72866b2266169
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-08-03 23:06:33 -0500

    Defining IVisitor and IVisitableElement interfaces and methods

--- Visitor_Pattern/Visitor.cs
+++ Visitor_Pattern/Visitor.cs
@@ -6,11 +6,28 @@ namespace Visitor_Pattern
 {
 	public interface IVisitor
 	{
-
+		/// <summary>
+		/// Visitor for Book Object
+		/// </summary>
+		/// <param name="book"></param>
+		void VisitBook(Book book);
+		/// <summary>
+		/// Visitor for Vinyl Object
+		/// </summary>
+		/// <param name="vinyl"></param>
+		void VisitVinyl(Vinyl vinyl);
+		/// <summary>
+		/// Helpful for debug
+		/// </summary>
+		void Print();
 	}
 
 	public interface IVisitableElement
 	{
-
+		/// <summary>
+		/// Method that accepts a visitor type
+		/// </summary>
+		/// <param name="visitor"></param>
+		void Accept(IVisitor visitor);
 	}
 }
