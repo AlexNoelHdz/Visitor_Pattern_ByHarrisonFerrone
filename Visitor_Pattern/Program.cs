d308f41af40e91a37b8c8625b46d40c1aa14a16c
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-08-03 23:52:06 -0500

    Implement Discount Visitor in a list of visitable objects

--- Visitor_Pattern/Program.cs
+++ Visitor_Pattern/Program.cs
@@ -6,14 +6,23 @@ namespace Visitor_Pattern
     class Program
     {
         static void Main(string[] args)
-        {
-            List<Object> items = new List<Object>
-            {
-                new Book(12345, 11.99),
-                new Book(78910, 22.79),
-                new Vinyl(11198, 17.99),
-                new Book(63254, 9.79)
-            };
-        }
-    }
+		{
+			List<IVisitableElement> items = new List<IVisitableElement>
+			{
+				new Book(12345, 11.99),
+				new Book(78910, 22.79),
+				new Vinyl(11198, 17.99),
+				new Book(63254, 9.79)
+			};
+
+			ImplementDiscountVisitor(items);
+		}
+
+		private static void ImplementDiscountVisitor(List<IVisitableElement> items)
+		{
+			var discountVisitor = new DiscountVisitor();
+			foreach (var item in items)
+				item.Accept(discountVisitor);
+		}
+	}
 }
