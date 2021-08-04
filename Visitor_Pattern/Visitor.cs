0be3c06b8cedfa77239109583a760f245de419df
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-08-03 23:48:08 -0500

    Defining a Discount Visitor to Extend the functionallity of visitabled objects

--- Visitor_Pattern/Visitor.cs
+++ Visitor_Pattern/Visitor.cs
@@ -30,4 +30,44 @@ namespace Visitor_Pattern
 		/// <param name="visitor"></param>
 		void Accept(IVisitor visitor);
 	}
+	/// <summary>
+	/// Adding Discount Functionality to visitable classes
+	/// </summary>
+	public class DiscountVisitor : IVisitor
+	{
+		/// <summary>
+		/// Acumulating data example
+		/// Savings on the objects we visited. 
+		/// </summary>
+		private double _savings { get; set; }
+
+		public void VisitBook(Book book)
+		{
+			var discount = 0.0;
+
+			if(book.Price < 20.00)
+			{
+				discount = book.GetDiscount(0.10);
+				Console.WriteLine($"Discounted: Book #{book.ID} is now ${Math.Round(book.Price - discount, 2)}");
+			}
+			else 
+			{
+				Console.WriteLine($"Full Price: Book #{book.ID} is ${book.Price}");
+			}
+
+			_savings += discount;
+		}
+
+		public void VisitVinyl(Vinyl vinyl)
+		{
+			var discount = vinyl.GetDiscount(0.15);
+			Console.WriteLine($"Super Savings: Vinyl #{vinyl.ID} is now ${Math.Round(vinyl.Price - discount, 2)}");
+			_savings += discount;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine($"\nYo saved a total of {Math.Round(_savings, 2)} on today's order!");
+		}
+	}
 }
